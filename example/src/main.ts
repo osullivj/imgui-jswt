3509964f5a223d15588cfd6e27c31667a81cf7e9
John O'Sullivan <osullivj@gmail.com>
2024-11-25 15:01:05 +0000

    NDWidget 1st cut

--- example/src/main.ts
+++ example/src/main.ts
@@ -3,7 +3,7 @@ import * as ImGui_Impl from "./imgui_impl.js";
 import { ShowDemoWindow } from "./imgui_demo.js";
 import { MemoryEditor } from "./imgui_memory_editor.js";
 
-let font: ImGui.Font | null = null;
+
 
 // Our state
 let show_demo_window: boolean = false;
@@ -41,10 +41,94 @@ export default async function main(): Promise<void> {
     }
 }
 
-async function AddFontFromFileTTF(url: string, size_pixels: number, font_cfg: ImGui.FontConfig | null = null, glyph_ranges: number | null = null): Promise<ImGui.Font> {
-    font_cfg = font_cfg || new ImGui.FontConfig();
-    font_cfg.Name = font_cfg.Name || `${url.split(/[\\\/]/).pop()}, ${size_pixels.toFixed(0)}px`;
-    return ImGui.GetIO().Fonts.AddFontFromMemoryTTF(await LoadArrayBuffer(url), size_pixels, font_cfg, glyph_ranges);
+
+
+// Base widget
+abstract class NDWidget {
+    nd_type:string;
+    // TODO: possible optimisation to presize child array correctly
+    // from layout.json 
+    children:NDWidget[] = [];
+
+    constructor(readonly ndtype:string) {
+        this.nd_type = ndtype;
+    }
+    
+    abstract render(): void;
+    
+    render_children(): void {
+        
+    }
+}
+
+class NDHome extends NDWidget {
+    constructor(readonly ndtype:string) {
+        super(ndtype);
+    }
+    
+    render(): void {
+        ImGui.Begin(_nd_ctx.layout === null ? "nodom" : _nd_ctx.layout.home.title);     
+        this.render_children();
+        ImGui.End();
+    }
+}
+
+class NDInputInt extends NDWidget {
+    constructor(readonly ndtype:string) {
+        super(ndtype);
+    }
+    
+    render(): void {
+    }
+}
+
+
+class NDLabel extends NDWidget {
+    constructor(readonly ndtype:string) {
+        super(ndtype);
+    }
+    
+    render(): void {
+    }
+}
+
+class NDHomeFooter extends NDWidget {
+    constructor(readonly ndtype:string) {
+        super(ndtype);
+    }
+    
+    render(): void {
+        // static float f = 0.0f;
+        // static int counter = 0;
+
+ 
+        // main GUI footer
+        ImGui.Text(`Application average ${(1000.0 / ImGui.GetIO().Framerate).toFixed(3)} ms/frame (${ImGui.GetIO().Framerate.toFixed(1)} FPS)`);
+        ImGui.Checkbox("Memory Editor", (value = memory_editor.Open) => memory_editor.Open = value);
+        ImGui.SameLine();
+        ImGui.Checkbox("Demo Window", (value = show_demo_window) => show_demo_window = value);      // Edit bools storing our windows open/close state        
+        if (memory_editor.Open)
+            memory_editor.DrawWindow("Memory Editor", ImGui.bind.HEAP8.buffer);
+        const mi: ImGui.Bind.mallinfo = ImGui.bind.mallinfo();
+        // ImGui.Text(`Total non-mmapped bytes (arena):       ${mi.arena}`);
+        // ImGui.Text(`# of free chunks (ordblks):            ${mi.ordblks}`);
+        // ImGui.Text(`# of free fastbin blocks (smblks):     ${mi.smblks}`);
+        // ImGui.Text(`# of mapped regions (hblks):           ${mi.hblks}`);
+        // ImGui.Text(`Bytes in mapped regions (hblkhd):      ${mi.hblkhd}`);
+        ImGui.Text(`Max. total allocated space (usmblks):  ${mi.usmblks}`);
+        // ImGui.Text(`Free bytes held in fastbins (fsmblks): ${mi.fsmblks}`);
+        ImGui.Text(`Total allocated space (uordblks):      ${mi.uordblks}`);
+        ImGui.Text(`Total free space (fordblks):           ${mi.fordblks}`);
+        
+        if (_nd_ctx.font) {
+            ImGui.PushFont(_nd_ctx.font);
+            ImGui.Text(`${_nd_ctx.font.GetDebugName()}`);
+            if (_nd_ctx.font.FindGlyphNoFallback(0x5929)) {
+                ImGui.Text(`U+5929: \u5929`);
+            }
+            ImGui.PopFont();
+        }
+    }
 }
 
 class NDMessage {
@@ -60,11 +144,19 @@ class NDContext {
     decoder: any;
     encoder: any;
     layout: any;
-    cache: Map<string, any>;
+    cache: Map<string, any> = new Map<string, any>();
+    wctor: Map<string, any> = new Map<string, any>([
+            ["InputInt", NDInputInt],
+            ["Label", NDLabel]
+        ]);      // widget constructors
+    stack: any[] = [];
     data_type_u8: Uint8Array;     // was used by InputScalar for RV
     // consts
     update_interval: number;
-    init_interval: number;    
+    init_interval: number;
+    // fonts
+    font: ImGui.Font|null;
+    io: ImGui.IO = ImGui.GetIO();
     
     
     constructor() {
@@ -75,8 +167,18 @@ class NDContext {
         this.update_interval = 50;
         this.init_interval = 1000;
         // stuff that will be supplied later
+        this.font = null;
         this.layout = null;
-        this.cache = new Map<string, any>();
+        // this.stack = [];
+        // this.cache = new Map<string, any>();
+        // this.wctor = 
+    }
+
+    async load_font_ttf(url: string, size_pixels: number, font_cfg: ImGui.FontConfig | null = null, glyph_ranges: number | null = null): Promise<ImGui.Font> {
+        this.io.Fonts.AddFontDefault();
+        font_cfg = font_cfg || new ImGui.FontConfig();
+        font_cfg.Name = font_cfg.Name || `${url.split(/[\\\/]/).pop()}, ${size_pixels.toFixed(0)}px`;
+        return this.io.Fonts.AddFontFromMemoryTTF(await LoadArrayBuffer(url), size_pixels, font_cfg, glyph_ranges);
     }
     
     async init(): Promise<number> {
@@ -101,9 +203,17 @@ class NDContext {
         const cache_json = await cache_response.text();
         console.log('NDContext.init: ' + cache_json);
         this.cache = await JSON.parse(cache_json);
+        
+        // Load font
+        this.font = await this.load_font_ttf("../imgui/misc/fonts/Roboto-Medium.ttf", 16.0);
+        ImGui.ASSERT(this.font !== null);
+
+        // Finally, tee up the first element in layout to render: home
+        this.push(this.layout.home);
 
         return 0;
     }
+    
     
     on_open(ev: any): void {
         console.log("Websock connected");
@@ -118,6 +228,16 @@ class NDContext {
         console.log('NDContext.update: ' + ev.data);
         let data:any = JSON.parse(ev.data);
         // cache update code here...
+    }
+    
+    render() {
+    }
+    
+    push(layout_element:any) {
+    }
+    
+    pop()
+    {
     }
  
 }
@@ -149,14 +269,14 @@ async function _init(): Promise<void> {
     // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
     // - Read 'docs/FONTS.md' for more instructions and details.
     // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-    io.Fonts.AddFontDefault();
-    font = await AddFontFromFileTTF("../imgui/misc/fonts/Roboto-Medium.ttf", 16.0);
+    // io.Fonts.AddFontDefault();
+    // font = await AddFontFromFileTTF("../imgui/misc/fonts/Roboto-Medium.ttf", 16.0);
     // font = await AddFontFromFileTTF("../imgui/misc/fonts/Cousine-Regular.ttf", 15.0);
     // font = await AddFontFromFileTTF("../imgui/misc/fonts/DroidSans.ttf", 16.0);
     // font = await AddFontFromFileTTF("../imgui/misc/fonts/ProggyTiny.ttf", 10.0);
     // font = await AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0, null, io.Fonts.GetGlyphRangesJapanese());
     // font = await AddFontFromFileTTF("https://raw.githubusercontent.com/googlei18n/noto-cjk/master/NotoSansJP-Regular.otf", 18.0, null, io.Fonts.GetGlyphRangesJapanese());
-    ImGui.ASSERT(font !== null);
+    // ImGui.ASSERT(font !== null);
 
     // Setup Platform/Renderer backends
     // ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
@@ -196,56 +316,18 @@ function _loop(time: number): void {
     // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
     // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
     
-    // Handle any datagrams that have arrived since the last render.
-    // Yes, this is an async func, and we're invoking from a sync func,
-    // so we use .then() for a null op on completion.
-    // _h3readDatagrams().then(()=>{});
     
     // Start the Dear ImGui frame
     ImGui_Impl.NewFrame(time);
     ImGui.NewFrame();
 
-    // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+    // Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()!
+    // You can browse its code to learn more about Dear ImGui!).
     if (!done && show_demo_window) {
         done = /*ImGui.*/ShowDemoWindow((value = show_demo_window) => show_demo_window = value);
     }
 
-    // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
-    {
-        // static float f = 0.0f;
-        // static int counter = 0;
-        ImGui.Begin(_nd_ctx.layout === null ? "nodom" : _nd_ctx.layout.home.title);
- 
-        // main GUI footer
-        ImGui.Text(`Application average ${(1000.0 / ImGui.GetIO().Framerate).toFixed(3)} ms/frame (${ImGui.GetIO().Framerate.toFixed(1)} FPS)`);
-        ImGui.Checkbox("Memory Editor", (value = memory_editor.Open) => memory_editor.Open = value);
-        ImGui.SameLine();
-        ImGui.Checkbox("Demo Window", (value = show_demo_window) => show_demo_window = value);      // Edit bools storing our windows open/close state        
-        if (memory_editor.Open)
-            memory_editor.DrawWindow("Memory Editor", ImGui.bind.HEAP8.buffer);
-        const mi: ImGui.Bind.mallinfo = ImGui.bind.mallinfo();
-        // ImGui.Text(`Total non-mmapped bytes (arena):       ${mi.arena}`);
-        // ImGui.Text(`# of free chunks (ordblks):            ${mi.ordblks}`);
-        // ImGui.Text(`# of free fastbin blocks (smblks):     ${mi.smblks}`);
-        // ImGui.Text(`# of mapped regions (hblks):           ${mi.hblks}`);
-        // ImGui.Text(`Bytes in mapped regions (hblkhd):      ${mi.hblkhd}`);
-        ImGui.Text(`Max. total allocated space (usmblks):  ${mi.usmblks}`);
-        // ImGui.Text(`Free bytes held in fastbins (fsmblks): ${mi.fsmblks}`);
-        ImGui.Text(`Total allocated space (uordblks):      ${mi.uordblks}`);
-        ImGui.Text(`Total free space (fordblks):           ${mi.fordblks}`);
-        
-        if (font) {
-            ImGui.PushFont(font);
-            ImGui.Text(`${font.GetDebugName()}`);
-            if (font.FindGlyphNoFallback(0x5929)) {
-                ImGui.Text(`U+5929: \u5929`);
-            }
-            ImGui.PopFont();
-        }
-
-        ImGui.End();
-    }
-
+    _nd_ctx.render();   
 
     ImGui.EndFrame();
 
