d05d55005ccf72283048c6f1b73dbe3ce5342f49
John O'Sullivan <osullivj@gmail.com>
2024-12-29 17:20:52 +0000

    renaming to _module and adding parquet loading

--- example/duck_module.js
+++ example/duck_module.js
@@ -13,7 +13,7 @@ let duck_conn = null;
 // This was in index.html: by doing it here we avoid having to pass the
 // DB handle across threads. NB this module must be pulled in to index.html
 // like so...
-// <script type="module" src="./duck_handler.js" defer></script>
+// <script type="module" src="./duck_module.js" defer></script>
 import * as duck from "https://cdn.jsdelivr.net/npm/@duckdb/duckdb-wasm@latest/+esm";
 const JSDELIVR_BUNDLES = duck.getJsDelivrBundles();
 const bundle = await duck.selectBundle(JSDELIVR_BUNDLES);
@@ -27,16 +27,29 @@ duck_db = new duck.AsyncDuckDB(logger, db_worker);
 await duck_db.instantiate(bundle.mainModule, bundle.pthreadWorker);
 // revoke the object url now no longer needed
 URL.revokeObjectURL(db_worker_url);
-console.log("duck_handler.js: DuckDB instantiated ", db_worker_url);
+console.log("duck_module.js: DuckDB instantiated ", db_worker_url);
 window.__nodom__ = {duck_module:self};
 
 async function exec_duck_db_query(sql) {
     if (!duck_db) {
-        console.error("duck_handler:DuckDB-Wasm not initialized");
+        console.error("duck_module:DuckDB-Wasm not initialized");
         return;
     }
     if (!duck_conn) {
-        console.log("duck_handler:reconnecting...");
+        console.log("duck_module:reconnecting...");
+        duck_conn = await duck_db.connect();
+    }
+    const arrow_table = await duck_conn.query(sql);
+    return arrow_table;
+}
+
+async function load_parquet(url) {
+    if (!duck_db) {
+        console.error("duck_module:DuckDB-Wasm not initialized");
+        return;
+    }
+    if (!duck_conn) {
+        console.log("duck_module:reconnecting...");
         duck_conn = await duck_db.connect();
     }
     const arrow_table = await duck_conn.query(sql);
@@ -51,9 +64,9 @@ self.onmessage = async (event) => {
         case "query":
             let arrow_table = await exec_duck_db_query(nd_db_request.payload);
             const cols = arrow_table.schema.fields.map((field) => field.name);
-            console.log("duck_handler cols:", cols);
+            console.log("duck_module cols:", cols);
             const rows = arrow_table.toArray();
-            console.log("duck_handler rows:", rows);
+            console.log("duck_module rows:", rows);
             // postMessage({rtype:"query_result", schema:cols, row_count:arrow_table.numRows, query:nd_db_request.payload});
             postMessage({rtype:"query_result", payload:arrow_table});
             break;
